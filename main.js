65fb0f86b69d3caa0f59a34253da16fc9b12aa73
jennycai01 <cai.je@northeastern.edu>
2023-01-30 16:32:48 -0500

    button is semi functional

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 
-
+let currCount = 0;
 function buttonClicked() {
 	console.log("button was clicked");
 
 	// select the element we want to modify
 	let count = document.getElementById("count");
 
-	count.innerHTML = count + 1
+	let buttonDiv = document.getElementById("button-div");
+
+
+	count.innerHTML = currCount + 1;
 }
 
 
