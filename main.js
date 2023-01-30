c77218bbc1642a413c07ef33fddd1a0e1578ff2e
macintosh air <macintoshair@MacBook-Pro.local>
2023-01-30 16:21:37 -0500

    we tried

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 
 
+function buttonClicked() {
+	console.log("button was clicked");
+
+	// select the element we want to modify
+	let count = document.getElementById("count");
+
+	count.innerHTML = count + 1
+}
+
 
